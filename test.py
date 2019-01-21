968b2a67ab41ccbb3dfccd5abc6659e282de1163
Mateusz Adamowicz <mateusz.adamowicz.1994@gmail.com>
2019-01-21 13:56:10 +0100

    2

--- test.py
+++ test.py
@@ -1,27 +1,34 @@
 import tensorflow as tf
 import datetime
 
-Input = tf.placeholder('float', shape=[None, 2], name="Input")
 Target = tf.placeholder('float', shape=[None, 1], name="Target")
-inputBias = tf.Variable(initial_value=tf.random_normal(shape=[1], stddev=0.4, dtype='float', name="input_bias"))
 
-weights = tf.Variable(initial_value=tf.random_normal(shape=[2, 3], stddev=0.4, dtype='float', name="hidden_weights"))
-hiddenBias = tf.Variable(initial_value=tf.random_normal(shape=[1], stddev=0.4, dtype='float', name="hidden_bias"))
-tf.summary.histogram(name="Weights_1", values=weights)
+with tf.name_scope("Input_Layer") as scope:
+    Input = tf.placeholder('float', shape=[None, 2], name="Input")
+    inputBias = tf.Variable(initial_value=tf.random_normal(shape=[1], stddev=0.4, dtype='float', name="input_bias"))
 
-outputWeigths = tf.Variable(initial_value=tf.random_normal(shape=[3, 1], stddev=0.4, dtype='float', name="output_weights"))
-tf.summary.histogram(name="Weights_2", values=outputWeigths)
+with tf.name_scope("Hidden_Layer") as scope:
+    weights = tf.Variable(initial_value=tf.random_normal(shape=[2, 3], stddev=0.4, dtype='float', name="hidden_weights"))
+    hiddenBias = tf.Variable(initial_value=tf.random_normal(shape=[1], stddev=0.4, dtype='float', name="hidden_bias"))
+    tf.summary.histogram(name="Weights_1", values=weights)
+    hiddenLayer = tf.matmul(Input, weights) + inputBias
+    hiddenLayer = tf.sigmoid(hiddenLayer, name="hidden_layer_activation")
 
-hiddenLayer = tf.matmul(Input, weights) + inputBias
-hiddenLayer = tf.sigmoid(hiddenLayer, name="hidden_layer_activation")
+with tf.name_scope("Output_Layer") as scope:
+    outputWeigths = tf.Variable(initial_value=tf.random_normal(shape=[3, 1], stddev=0.4, dtype='float', name="output_weights"))
+    tf.summary.histogram(name="Weights_2", values=outputWeigths)
+    output = tf.matmul(hiddenLayer, outputWeigths) + hiddenBias
+    output = tf.sigmoid(output, name="output_activation")
 
-output = tf.matmul(hiddenLayer, outputWeigths) + hiddenBias
-output = tf.sigmoid(output, name="output_activation")
+with tf.name_scope("Optimizer") as scope:
+    cost = tf.squared_difference(Target, output)
+    cost = tf.reduce_mean(cost)
+    tf.summary.scalar("error_1", cost)
+    tf.summary.scalar("error_2", cost)
+    tf.summary.scalar("lr_1", cost)
+    tf.summary.scalar("lr_2", cost)
 
-cost = tf.squared_difference(Target, output)
-cost = tf.reduce_mean(cost)
-tf.summary.scalar("error", cost)
-optimizer = tf.train.AdamOptimizer().minimize(cost)
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 inp = [[1, 1], [1, 0], [0, 1], [0, 0]]
 out = [[0], [1], [1], [0]]
